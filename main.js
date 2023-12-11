187ad307c30733929a25c9151cde5213cc8395e6
KungFuKelo <116620255+KungFuKelo@users.noreply.github.com>
2023-12-12 01:35:46 +0200

    total revamp
    -  face left
    - book info changes
    - code refractor

--- main.js
+++ main.js
@@ -1,56 +1,138 @@
 // DOM elements
-var bookCountView = document.getElementsByClassName("book-count")[0];
-var bookCounInp = document.getElementById("bookCount");
+//// Book count view:
+var countView = document.getElementsByClassName("book-count")[0];
+var countInp = document.getElementsByName("bookCount")[0];
 var countBtn = document.getElementById("countBtn");
-var countInvalid = document.getElementById("bookCountInvalid");
-var bookInfoView = document.getElementsByClassName("book-info")[0];
-var bookInfoTitle = document.getElementById("bookInfoTitle");
+var countValid = document.getElementById("countInvalid");
+//// Book info view:
+var infoView = document.getElementsByClassName("book-info")[0];
+var infoTitle = document.getElementById("bookInfoTitle");
+var progress = document.getElementsByClassName("progress")[0];
+var progressEnd = document.getElementsByClassName("progress-end")[0];
+var titleInp = document.getElementsByName("bookName")[0];
+var authorInp = document.getElementsByName("bookAuth")[0];
+var pagesInp = document.getElementsByName("bookPages")[0];
+var catInp = document.getElementsByName("bookCat")[0];
 var infoBtn = document.getElementById("infoBtn");
-var bookNameInp = document.getElementsByName("bookName")[0];
-var bookAuthInp = document.getElementsByName("bookAuth")[0];
-var bookAuthMailInp = document.getElementsByName("bookAuthEmail")[0];
-var bookPriceInp = document.getElementsByName("bookPrice")[0];
-var bookTableView = document.getElementsByClassName("book-table")[0];
-var bookTableBody = document.getElementsByTagName("tbody")[0];
+var infoSkipBtn = document.getElementById("infoSkipBtn");
+var infoInvalid = document.getElementById("infoInvalid");
+//// Book gallery view:
+var listView = document.getElementsByClassName("book-list")[0];
+
+// Book constructor
+function Book(t, a, p, c) {
+  this.bookTitle = t;
+  this.bookAuth = a;
+  this.bookPages = p;
+  this.bookCat = c;
+}
 
 // Book-related variables
-var bookCountNo;
+var bookCount;
 var bookArr = [];
-var addCount = 0;
-var nameRegex = /^[A-Za-z\s'.-]+$/;
-var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+var clickCount = 0;
+var textRegex = /^[A-Za-z\s'.-]+$/;
 
 // Event listeners
 countBtn.addEventListener("click", handleCountBtn);
 infoBtn.addEventListener("click", handleInfoBtn);
+infoSkipBtn.addEventListener("click", handleInfoSkipBtn);
 
 // Event listeners functions
 function handleCountBtn() {
-  if (bookCounInp.value === "" || bookCounInp.value > 6) {
-    displayCountError();
-  } else {
-    bookCountNo = bookCounInp.value;
-    hideElement(bookCountView);
-    showElement(bookInfoView);
+  if (handelCountValidation()) {
+    bookCount = countInp.value;
+    updateBookInfoTitle();
+    hideElement(countView);
+    showElement(infoView);
   }
 }
 
 function handleInfoBtn() {
   resetInputValidity();
-
-  if (isInvalidBookInfo()) {
-    handleInvalidBookInfo();
-  } else {
-    handleValidBookInfo();
+  if (handelInfoValidation()) {
+    createNewBook();
+    clearInputFields();
+    clickCount++;
+    updateBookInfoTitle();
+    progress.style.width = `calc(${(clickCount / bookCount) * 100}%)`;
+    if (clickCount == bookCount) {
+      progressEnd.style.width = "100%";
+      createBookList();
+      setTimeout(() => {
+        hideElement(infoView);
+        showElement(listView);
+      }, 900);
+    }
   }
 }
 
-// Helper functions
-function displayCountError() {
-  bookCounInp.style.outline = "1px solid red";
-  countInvalid.style.visibility = "visible";
+function handleInfoSkipBtn() {
+  clearInputFields();
+  progressEnd.style.width = "100%";
+  createBookList();
+  setTimeout(() => {
+    hideElement(infoView);
+    showElement(listView);
+  }, 900);
 }
 
+// Validation functions
+function handelCountValidation() {
+  if (countInp.value === "" || countInp.value > 6) {
+    countInp.style.outline = "1px solid red";
+    countValid.style.visibility = "visible";
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function handelInfoValidation() {
+  if (titleInp.value === "" || isFinite(titleInp.value)) {
+    titleInp.classList.add("invalid");
+    infoInvalid.style.visibility = "visible";
+    infoInvalid.innerText = "Please enter a valid book title";
+    return false;
+  } else if (!textRegex.test(authorInp.value)) {
+    authorInp.classList.add("invalid");
+    infoInvalid.style.visibility = "visible";
+
+    infoInvalid.innerText = "Please enter a valid author name";
+    return false;
+  } else if (isNaN(pagesInp.value) || pagesInp.value === "") {
+    pagesInp.classList.add("invalid");
+    infoInvalid.style.visibility = "visible";
+
+    infoInvalid.innerText = "Please enter valid page number";
+    return false;
+  } else if (!textRegex.test(catInp.value)) {
+    catInp.classList.add("invalid");
+    infoInvalid.style.visibility = "visible";
+
+    infoInvalid.innerText = "Please enter a valid book category";
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function clearInputFields() {
+  titleInp.value = "";
+  authorInp.value = "";
+  pagesInp.value = "";
+  catInp.value = "";
+}
+
+function resetInputValidity() {
+  titleInp.classList.remove("invalid");
+  authorInp.classList.remove("invalid");
+  pagesInp.classList.remove("invalid");
+  catInp.classList.remove("invalid");
+  infoInvalid.style.visibility = "hidden";
+}
+
+// Helper functions
 function hideElement(element) {
   element.style.display = "none";
 }
@@ -59,145 +141,41 @@ function showElement(element) {
   element.style.display = "flex";
 }
 
-function resetInputValidity() {
-  bookNameInp.classList.remove("invalid");
-  bookAuthInp.classList.remove("invalid");
-  bookAuthMailInp.classList.remove("invalid");
-  bookPriceInp.classList.remove("invalid");
-}
-
-function isInvalidBookInfo() {
-  return (
-    bookNameInp.value === "" ||
-    isFinite(bookNameInp.value) ||
-    !nameRegex.test(bookAuthInp.value) ||
-    !emailRegex.test(bookAuthMailInp.value) ||
-    bookPriceInp.value === "" ||
-    isNaN(bookPriceInp.value)
+function createNewBook() {
+  var newBook = new Book(
+    titleInp.value,
+    authorInp.value,
+    pagesInp.value,
+    catInp.value
   );
-}
-
-function handleInvalidBookInfo() {
-  bookNameInp.classList.add("invalid");
-}
-
-function handleValidBookInfo() {
-  bookArr.push(
-    new Book(
-      bookNameInp.value,
-      bookAuthInp.value,
-      bookAuthMailInp.value,
-      bookPriceInp.value
-    )
-  );
-  addCount++;
-  updateBookInfoTitle();
-  clearInputFields();
-
-  if (addCount == bookCountNo) {
-    displayBookTable();
-  }
+  bookArr.push(newBook);
 }
 
 function updateBookInfoTitle() {
-  bookInfoTitle.innerHTML = `Enter book ${addCount + 1} details`;
-}
-
-function clearInputFields() {
-  bookNameInp.value = "";
-  bookAuthInp.value = "";
-  bookAuthMailInp.value = "";
-  bookPriceInp.value = "";
-}
-
-function displayBookTable() {
-  for (var n = 0; n < bookArr.length; n++) {
-    var tr = createTableRow(bookArr[n]);
-    createEditAndDeleteButtons(tr, n);
-    addTableRowToTableBody(tr);
-    addDeleteButtonListener(delBtn, tr, n);
-    addEditButtonListener(editBtn, tr);
-  }
-
-  hideElement(bookInfoView);
-  showElement(bookTableView);
-}
-
-function createTableRow(book) {
-  var tr = document.createElement("tr");
-  for (var key in book) {
-    var td = document.createElement("td");
-    td.innerHTML = book[key];
-    tr.append(td);
-  }
-  return tr;
-}
-
-function createEditAndDeleteButtons(tr, n) {
-  edit = document.createElement("td");
-  editBtn = createButton("Edit");
-  del = document.createElement("td");
-  delBtn = createButton("Delete");
-  editBtn.classList.add("btn");
-  delBtn.classList.add("btn");
-  edit.append(editBtn);
-  del.append(delBtn);
-  tr.append(edit);
-  tr.append(del);
-}
-
-function createButton(text) {
-  var button = document.createElement("button");
-  button.innerHTML = text;
-  return button;
-}
-
-function addTableRowToTableBody(tr) {
-  bookTableBody.append(tr);
-}
-
-function addDeleteButtonListener(delBtn, tr, n) {
-  delBtn.addEventListener("click", function () {
-    bookArr.splice(n, 1);
-    tr.remove();
-    console.log(bookArr);
-  });
-}
-
-function addEditButtonListener(editBtn, tr) {
-  editBtn.addEventListener("click", function () {
-    toggleEditButtonState(editBtn, tr);
-  });
-}
-
-function toggleEditButtonState(editBtn, tr) {
-  if (editBtn.innerHTML === "Edit") {
-    editBtn.innerHTML = "Done";
-    editTableRow(tr);
+  if (clickCount + 1 <= bookCount) {
+    infoTitle.innerHTML = `📚 Enter book ( ${clickCount + 1} / ${bookCount} )`;
   } else {
-    editBtn.innerHTML = "Edit";
-    updateTableRow(tr);
+    infoTitle.innerHTML = `📚 Enter book ✅`;
   }
 }
 
-function editTableRow(tr) {
-  for (var k = 0; k < tr.children.length - 2; k++) {
-    var currentVal = tr.children[k].innerHTML;
-    tr.children[k].innerHTML = `<input type="text" value=${currentVal} />`;
-  }
-}
+// Creating book list
 
-function updateTableRow(tr) {
-  for (var x = 0; x < tr.children.length - 2; x++) {
-    var currentVal = tr.children[x].firstChild.value;
-    tr.children[x].innerHTML = currentVal;
+function createBookList() {
+  for (var i = 0; i < bookArr.length; i++) {
+    var book = document.createElement("div");
+    book.classList.add("book");
+    (function () {
+      for (var key in bookArr[i]) {
+        var data = document.createElement("div");
+        data.classList.add("book-data");
+        if (key == "bookCat") {
+          data.classList.add("book-cat");
+        }
+        data.innerText = bookArr[i][key];
+        book.append(data);
+      }
+    })();
+    listView.append(book);
   }
-}
-
-// Book constructor
-function Book(n, a, m, p) {
-  this.bookName = n;
-  this.bookAuth = a;
-  this.authMail = m;
-  this.bookPrice = p;
 }
